81656ce6170e88db5fcfec9df461c9050521c07d
Kjell Jorner <kjell.jorner@gmail.com>
2021-10-04 21:08:12 -0400

    Add requirements and Python version to setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,13 @@ To run the package as standalone
 from setuptools import setup
  
 setup(
-  name='geodesic_interpolate',
+  name='geodesic-interpolate',
   version='1.0.0',
   description='Interpolation and smoothing of reaction paths with geodesics in '
               'redundant internal coordinates.',
   packages=['geodesic_interpolate'],
+  install_requires=["numpy", "scipy"],
+  python_requires=">=3.8",  
   entry_points = {
     'console_scripts': [
       'geodesic_interpolate=geodesic_interpolate.__main__:main',
